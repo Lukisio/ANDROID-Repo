010f4d2de22291273d296db4597056412dc6e8d2
Łukasz Pająk <lukisio@siedemjeden.net>
2014-01-07 01:25:36 +0100

    :)

--- UnitedCoCalculator/src/com/lukis/unitedcocalculator/MainActivity.java
+++ UnitedCoCalculator/src/com/lukis/unitedcocalculator/MainActivity.java
@@ -1,46 +1,79 @@
 package com.lukis.unitedcocalculator;
 
 
-import java.text.DecimalFormat;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.text.ClipboardManager;
+import android.text.Html;
 import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	
-	private TextView ekran1, ekran2, ekran3, ekranError;
-	double input1, output1, output2, x;
+	private TextView email, input1, input2, input3, output1, output2, output3, ekranError;
+	double x;
+	RelativeLayout frontBackground;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ekran1 = (TextView) findViewById(R.id.editText1);
-		ekran2 = (TextView) findViewById(R.id.textView02);
-		ekran3 = (TextView) findViewById(R.id.textView03);
+		frontBackground = (RelativeLayout) findViewById(R.id.mainLayout);
+		input1 = (TextView) findViewById(R.id.editText1);
+		input2 = (TextView) findViewById(R.id.editText2);
+		input3 = (TextView) findViewById(R.id.editText3);
+		output1 = (TextView) findViewById(R.id.textView10);
+		output2 = (TextView) findViewById(R.id.textView11);
+		output3 = (TextView) findViewById(R.id.textView12);
 		ekranError = (TextView) findViewById(R.id.TextView000);
+		email = (TextView) findViewById(R.id.textView5);
 		
-        ekran1.setOnKeyListener(new OnKeyListener() {
-			
+		email.setText(Html.fromHtml("امجد سلامة e-mail: <u><font color=blue>aasalame@yahoo.com</font></u>"));
+        input1.setOnKeyListener(new OnKeyListener() {
         	public boolean onKey(View v, int keyCode, KeyEvent event) {
-				// if keydown and "enter" is pressed
 				if ((event.getAction() == KeyEvent.ACTION_DOWN)
 					&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
-
 				wylicz();
 				findViewById(R.id.mainLayout).requestFocus();
 	            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-	            imm.hideSoftInputFromWindow(ekran1.getWindowToken(), 0);
+	            imm.hideSoftInputFromWindow(input1.getWindowToken(), 0);
+				}
+				return false;
+			}
+        });
+		
+        input2.setOnKeyListener(new OnKeyListener() {
+        	public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN)
+					&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+				wylicz();
+				findViewById(R.id.mainLayout).requestFocus();
+	            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+	            imm.hideSoftInputFromWindow(input2.getWindowToken(), 0);
+				}
+				return false;
+			}
+        });
+		
+        input3.setOnKeyListener(new OnKeyListener() {
+        	public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN)
+					&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+				wylicz();
+				findViewById(R.id.mainLayout).requestFocus();
+	            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+	            imm.hideSoftInputFromWindow(input3.getWindowToken(), 0);
 				}
 				return false;
 			}
@@ -48,35 +81,69 @@ public class MainActivity extends Activity {
 	}
 
 	
+	public void kopiuj(View view){
+		 ClipboardManager cm=(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+		 cm.setText("سعر الشراء: " + input1.getText().toString() + "د " +
+				 "\nالدفعة الأولى: " + input2.getText().toString() + "د " +
+				 "\nالقسط الشهري: " + input3.getText().toString() + "د " +
+				 "\nسعر البيع: " + output1.getText().toString() + 
+				 "\nعدد الاقساط: " + output2.getText().toString() 
+			//	 +"الربح الشهري " + output3.getText().toString()
+				 ); // " سعر الشراء: 200 د"
+	}
+	
 	public void wylicz() {
 	
-		
-		
-	   	if (checkEmpty(ekran1)) {
-			x = Float.valueOf(ekran1.getText().toString());
-		    if (x>999999) {
-		   		ekran2.setText(" ");
-		   		ekran3.setText(" ");
-		   		ekranError.setText("قيمة السلعة out of range");
+	   	if (checkEmpty(input1)) {
+   		if (checkEmpty(input2)) {
+		if (checkEmpty(input3)) {
+			double valInput1 = Float.valueOf(input1.getText().toString());
+			double valInput2 = Float.valueOf(input2.getText().toString());
+			double valInput3 = Float.valueOf(input3.getText().toString());
+		    if (valInput1>999999||valInput2>999999||valInput3>999999) {
+		    	clear();
+		    	
+		    	frontBackground.setBackgroundColor(0x50ff0000);
+		   		ekranError.setText("out of range");
 		    } else {
-				double valOutput1 = (double) (x+0.6*x)/12;
-				double valOutput2 = (double) x * 12;
-				ekran2.setText(" "+ (int) Math.ceil(valOutput1));
-				ekran3.setText(" "+ valOutput2);
+//		    	double procent;
+//		    	if (valInput1<360) procent=0.75; else procent=0.6; 
+				double valOutput1 = (double) (valInput1*0.65+valInput1);
+				double valOutput2 = (double) ((valOutput1-valInput2)/valInput3);
+				double valOutput3 = (double) ((valOutput1-valInput1)/((valOutput1-valInput2)/valInput3));				
+				double valScreen = (double) ((valInput1-valInput2)*0.45+valInput1-valInput2)/valInput3; 
+				output1.setText( (int) Math.ceil(valOutput1) +"د ");
+				output2.setText( (int) Math.ceil(valOutput2) +"شهر " );
+				if (Math.floor(valScreen)>13) {frontBackground.setBackgroundColor(0x50ff0000);} else {frontBackground.setBackgroundColor(0x5000a100);}
+				output3.setText( (int) Math.round(valOutput3) +"د " );
 				ekranError.setText(" ");
 		    }		
-	   	} else {
-	   		ekran2.setText(" ");
-	   		ekran3.setText(" ");
-	   		ekranError.setText(" ");
-	   	}
+	   	} else { clear();}
+		} else { clear();}
+   		} else { clear();}
 	
-	//	Output1= roundup[ (x+0.6*x)/12 , 0]
-	//	Output2= Output1*12
 		return;
 	}
 	
+	public void clear(){
+   		output1.setText(" ");
+   		output2.setText(" ");
+   		output3.setText(" ");
+   		frontBackground.setBackgroundColor(0x00ff0000);
+   		ekranError.setText(" ");
+	}
 	
+	public void email(View view) {
+    	Intent i = new Intent(Intent.ACTION_SEND);
+    	i.setType("message/rfc822");
+    	i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"aasalame@yahoo.com"});
+    	i.putExtra(Intent.EXTRA_SUBJECT, "Message");
+    	try {
+    	    startActivity(Intent.createChooser(i, "Select your e-mail program"));
+    	} catch (android.content.ActivityNotFoundException ex) {
+    	    Toast.makeText(MainActivity.this, "You don't have an e-mail program", Toast.LENGTH_SHORT).show();
+    	}
+	}
 	
     @Override // chowanie klawiatury po wciśnięciu w ekran
     public boolean dispatchTouchEvent(MotionEvent event) {
@@ -90,10 +157,7 @@ public class MainActivity extends Activity {
             w.getLocationOnScreen(scrcoords);
             float x = event.getRawX() + w.getLeft() - scrcoords[0];
             float y = event.getRawY() + w.getTop() - scrcoords[1];
-
-         //   Log.d("Activity", "Touch event "+event.getRawX()+","+event.getRawY()+" "+x+","+y+" rect "+w.getLeft()+","+w.getTop()+","+w.getRight()+","+w.getBottom()+" coords "+scrcoords[0]+","+scrcoords[1]);
             if (event.getAction() == MotionEvent.ACTION_UP && (x < w.getLeft() || x >= w.getRight() || y < w.getTop() || y > w.getBottom()) ) { 
-
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
                 wylicz();
